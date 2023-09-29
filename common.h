d5efc3ca92d5a11a8b2a94eac393243549904983
Malu <malulu1604@gmail.com>
2023-09-29 00:01:53 -0300

    Primeiros

---
+++ common.h
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "sys/types.h"  //No terminal do Linux, dar: man socket
+#include "sys/socket.h"
+
+//struct sockaddr{
+   // __u_short family;
+   // char *sa_data;
+//};
+
